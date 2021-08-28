01935d6bc7fc84d2a3a42fb70acbecb3fba73788
Michele Sanseviero <msanseviero1195@gmail.com>
2021-08-28 18:01:52 -0400

    add eslint rules

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const debug = require('debug')('index');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import DEBUG from 'debug';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import router from './routes/routes';
+
 const app = express();
-const router = require('./routes/routes');
+const debug = DEBUG('index');
 
 debug('connect with database:\n');
 
 const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200,
+  origin: '*',
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/', router);
 
-mongoose.connect(process.env.MONGO_URL,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }
-).then((db) => {
-    debug(`db is connect to ${db.connection.host}`);
+mongoose.connect(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}).then((db) => {
+  debug(`db is connect to ${db.connection.host}`);
 }).catch((err) => {
-    debug("error: ",err);
+  debug('error: ', err);
 });
 
-debug('Server listen in port: 3000');
+debug(`Server listen in port: ${process.env.SERVER_PORT}`);
 
-app.listen(3000,() => {
-    debug('Server on port: 3000');
+app.listen(process.env.SERVER_PORT, () => {
+  debug(`Server on port: ${process.env.SERVER_PORT}`);
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { mergeTypeDefs, mergeResolvers, makeExecutableSchema } = require('graphql-tools');
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { mergeTypeDefs, mergeResolvers, makeExecutableSchema } from 'graphql-tools';
 
 const router = express.Router();
 
@@ -8,14 +8,14 @@ const typeDefsMerge = mergeTypeDefs([]);
 const resolversMerge = mergeResolvers([]);
 
 const schema = makeExecutableSchema({
-    typeDefs: typeDefsMerge,
-    resolvers: resolversMerge,
+  typeDefs: typeDefsMerge,
+  resolvers: resolversMerge,
 });
 
-router.use('/chats',graphqlHTTP({
-    schema,
-    graphiql: true,
-    extensions: ({ context }) => context.bullJob?.remove(),
+router.use('/chats', graphqlHTTP({
+  schema,
+  graphiql: true,
+  extensions: ({ context }) => context.bullJob?.remove(),
 }));
 
-module.exports = router;
+export default router;
