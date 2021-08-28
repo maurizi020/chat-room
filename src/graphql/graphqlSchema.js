077dc529b8699ebb5663b76883f206ba377441ed
Michele Sanseviero <msanseviero1195@gmail.com>
2021-08-28 19:12:56 -0400

    create schema

---
+++ src/graphql/graphqlSchema.js
@@ -0,0 +1,44 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import queries from './resolvers/queries';
+import mutations from './resolvers/mutations';
+
+const resolvers = {
+  Query: queries,
+  Mutation: mutations,
+};
+
+const typeDefs = `
+
+  input userInput {
+    name: String
+    id: ID
+  }
+
+  type message {
+    timestamp: Float
+    message: String
+    typography: String
+    color: String
+  }
+
+  type chat {
+    recipient: ID
+    messages: [message]
+  }
+
+  type user {
+    name: String
+    chats: [chat]
+    id: ID
+  }
+
+  type Mutation {
+    createUser(input: userInput): user
+  }
+
+`;
+
+export default makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,12 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { mergeTypeDefs, mergeResolvers, makeExecutableSchema } from 'graphql-tools';
+import schema from '../graphql/graphqlSchema';
 
 const router = express.Router();
-
-const typeDefsMerge = mergeTypeDefs([]);
-const resolversMerge = mergeResolvers([]);
-
-const schema = makeExecutableSchema({
-  typeDefs: typeDefsMerge,
-  resolvers: resolversMerge,
-});
 
 router.use('/chats', graphqlHTTP({
   schema,
   graphiql: true,
-  extensions: ({ context }) => context.bullJob?.remove(),
 }));
 
 export default router;
