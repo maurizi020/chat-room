e1c7f8bc8402998dd3187d8a3347ea5901dbf121
Michele Sanseviero <msanseviero1195@gmail.com>
2021-08-29 21:29:05 -0400

    add addMessage mutation

--- src/graphql/resolvers/mutations.js
+++ src/graphql/resolvers/mutations.js
@@ -1,24 +1,17 @@
 import DEBUG from 'debug';
-import { randomBytes } from 'crypto';
-import { Users } from '../../models/models';
+import { pubsub } from './subscription';
+import Messages from '../../models/models';
 
 const debug = DEBUG('mutations');
-const size = 10;
 
 const mutations = {
-
-  setUser: async (_, { input }) => {
-    const dataUser = {
-      name: input.name,
-      id: randomBytes(64).toString('base64').slice(0, size),
-      chats: [],
-    };
-    const newUser = new Users(dataUser);
-    await newUser.save();
-    debug('setUser mutation, the new user is: ', newUser);
-    return newUser;
+  addMessage: async (_, { input }) => {
+    debug('Mutation addMessage input: ', input);
+    const newMessage = new Messages(input);
+    await newMessage.save();
+    pubsub.publish('chatRoom', { newMessage });
+    return newMessage;
   },
-
 };
 
 export default mutations;
