302d9a12559ae12abc70c4b5a64dd0b8829afd86
Michele Sanseviero <msanseviero1195@gmail.com>
2021-08-29 21:29:20 -0400

    add getMessages query

--- src/graphql/resolvers/queries.js
+++ src/graphql/resolvers/queries.js
@@ -1,5 +1,13 @@
-const queries = {
+import DEBUG from 'debug';
+import Messages from '../../models/models';
 
+const debug = DEBUG('queries');
+
+const queries = {
+  getMessages: () => {
+    debug('Query getMessage');
+    return Messages.find();
+  },
 };
 
 export default queries;
