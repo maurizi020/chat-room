041dced8e46783de53b3d56eab1a9e6a1a1f5e51
Michele Sanseviero <msanseviero1195@gmail.com>
2021-08-31 17:16:35 -0400

    update queries functions

--- src/graphql/resolvers/queries.js
+++ src/graphql/resolvers/queries.js
@@ -4,9 +4,10 @@ import Messages from '../../models/models';
 const debug = DEBUG('queries');
 
 const queries = {
-  getMessages: () => {
-    debug('Query getMessage');
-    return Messages.find();
+  getMessages: async () => {
+    const messageList = await Messages.find();
+    debug('Query getMessage', messageList);
+    return messageList;
   },
 };
 
