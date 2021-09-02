dafe0c5ecc5ec12efb58022032eedbed467a27c4
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-01 22:45:59 -0400

    add searchMessage query resolver

--- src/graphql/resolvers/queries.js
+++ src/graphql/resolvers/queries.js
@@ -6,8 +6,19 @@ const debug = DEBUG('queries');
 const queries = {
   getMessages: async () => {
     const messageList = await Messages.find();
-    debug('Query getMessage', messageList);
+    debug('Query getMessage, number of message: ', messageList.length);
     return messageList;
+  },
+  searchMessage: async (_, { input }) => {
+    const messageList = await Messages.find();
+    const resultList = [];
+    messageList.forEach((value) => {
+      if (value.text.indexOf(input) !== -1) {
+        resultList.push(value);
+      }
+    });
+    debug('Query searchMessage, number of message: ', resultList.length);
+    return resultList;
   },
 };
 
